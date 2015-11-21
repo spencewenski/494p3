8e6e83dbd380919ef0e74180dbf349ade0285c16
Spencer Ferris <sferris37@gmail.com>
2015-11-20 21:11:24 -0500

    add simple checkpoint system
    only moves player to a spawnpoint, doesn't save/set any other state

---
+++ Assets/Scripts/KillPlane.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillPlane : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag != "Player") {
+            return;
+        }
+        other.transform.position = Checkpoint.lastCheckpoint.spawnPoint.position;
+    }
+}
