656b44472a05c38c848bc5f9639d5983adbb1de9
ZucchiniSky <scottbommarito@gmail.com>
2015-11-22 17:40:55 -0500

    speed gun implemented

---
+++ Assets/Scripts/CubeSpeed.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeSpeed : Cube
+{
+    OutlinePulser outline;
+
+    void Awake()
+    {
+        // SET CUBE EFFECT IN YOUR AWAKE FUNCTION
+        effect = Cube.CubeEffect_e.SPEED;
+    }
+
+    void Start()
+    {
+        outline = gameObject.GetComponentInChildren<OutlinePulser>();
+    }
+
+    public override void doEffectChild(Collider collision)
+    {
+        outline.OutlinePulseOn = true;
+        outline.outlineColor = new Color(255, 204, 0);
+        outline.accentColor = Color.yellow;
+        tag = "speed";
+    }
+}
---
+++ Assets/Scripts/ProjectileSpeed.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileSpeed : Projectile
+{
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        // SET EFFECT IN YOUR AWAKE FUNCTION
+        effect = Cube.CubeEffect_e.SPEED;
+    }
+
+}
