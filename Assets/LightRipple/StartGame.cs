06efcb152404a242b3570af27dfe4046b8b25869
Jordan Liu <jminjie@umich.edu>
2015-11-22 23:34:10 -0500

    Start scene final and bigger door

---
+++ Assets/LightRipple/StartGame.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+	public string firstLevel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Return) || Input.GetKeyDown (KeyCode.KeypadEnter)) {
+			Destroy(GameObject.Find("Canvas"));
+			Application.LoadLevel(firstLevel);
+		}
+	}
+}
