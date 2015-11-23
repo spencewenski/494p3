3598bb9b8dd3a368f4de955c39136281cbe0c250
Spencer Ferris <sferris37@gmail.com>
2015-11-22 20:00:49 -0500

    add a script and prefab to switch to the next level

---
+++ Assets/Scripts/SwitchLevel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchLevel : MonoBehaviour {
+
+	public string nextLevel;
+
+	public bool ___________________;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag != "Player") {
+			return;
+		}
+		Application.LoadLevel(nextLevel);
+	}
+}
