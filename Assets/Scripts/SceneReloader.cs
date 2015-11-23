e7db562e3e292f75d9a0d0d6fd290fbd9adf7c46
Jordan Liu <jminjie@umich.edu>
2015-11-22 23:53:12 -0500

    scene reloader

---
+++ Assets/Scripts/SceneReloader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneReloader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.F12))
+		    Application.LoadLevel(Application.loadedLevel);
+	}
+}
