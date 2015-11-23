1d72cab46734e436361379888aeade656ff1bbbc
Spencer Ferris <sferris37@gmail.com>
2015-11-23 04:42:02 -0500

    add script to switch between levels

---
+++ Assets/Scripts/GotoLevel.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GotoLevel : MonoBehaviour {
+
+    public List<string> levels;
+
+	// Update is called once per frame
+	void Update () {
+        if (!Input.GetKey(KeyCode.RightShift)) {
+            return;
+        }
+	    if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            loadLevel(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            loadLevel(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            loadLevel(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4)) {
+            loadLevel(3);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5)) {
+            loadLevel(4);
+        }
+    }
+
+    public void loadLevel(int index) {
+        Application.LoadLevel(levels[index]);
+    }
+}
