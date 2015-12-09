43b781ab22580cf4be3dd99b20549e9dd59007ad
ZucchiniSky <scottbommarito@gmail.com>
2015-12-08 20:12:58 -0500

    delay before game can be started

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -3,16 +3,20 @@ using System.Collections;
 
 public class StartGame : MonoBehaviour {
 
+    public float secToWait = 2;
+
+    private float framesToWait;
+
 	public string firstLevel;
 
 	// Use this for initialization
 	void Start () {
-
+        framesToWait = secToWait / Time.fixedDeltaTime;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-        if (Input.anyKeyDown) {
+	void FixedUpdate () {
+        --framesToWait;
+        if (Input.anyKeyDown && framesToWait < 0) {
             Destroy(GameObject.Find("Canvas"));
             Application.LoadLevel(firstLevel);
 
