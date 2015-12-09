632e2e11bbcfacb0ad3a23cad92ecd4429727ccc
Spencer Ferris <sferris37@gmail.com>
2015-12-08 20:31:28 -0500

    improve switchlevel door thing
    - glows and has emissive color
    - also deleted _SceneRespawnTest

--- Assets/Scripts/SwitchLevel.cs
+++ Assets/Scripts/SwitchLevel.cs
@@ -7,6 +7,13 @@ public class SwitchLevel : MonoBehaviour {
 
 	public bool ___________________;
 
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.tag != "Player") {
+			return;
+		}
+		Application.LoadLevel(nextLevel);
+	}
+
 	void OnTriggerEnter(Collider other) {
 		if (other.tag != "Player") {
 			return;
