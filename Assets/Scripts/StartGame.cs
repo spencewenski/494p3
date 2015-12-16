f39e1818e57ae5abda273dc488d76005cf4d3cdb
foodmonkey <jasonphe@umich.edu>
2015-12-15 20:35:06 -0500

    shift + r to reset scores in start menu

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -11,22 +11,21 @@ public class StartGame : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		levelSelectionMenu = GameObject.Find ("LevelSelection");
-        bestTimes = levelSelectionMenu.transform.GetChild(3);
-        float totalTime = 0f;
-        for(int i = 1; i <= 5; i++)
-        {
-            float best = PlayerPrefs.GetFloat("Time" + i, 300);
-            totalTime += best;
-            bestTimes.GetChild(i - 1).gameObject.GetComponent<Text>().text = format(best);
-        }
-        
-        levelSelectionMenu.transform.GetChild(4).gameObject.GetComponent<Text>().text = "Total: " + format(totalTime);
+        showTimes();
         levelSelectionMenu.SetActive (false);
 	}
     
     // Update is called once per frame
     void Update () {
-		if (selectingLevel == false) {
+        if (Input.GetKey(KeyCode.RightShift) && Input.GetKey(KeyCode.R))
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                PlayerPrefs.SetFloat("Time" + i, 300);
+            }
+            showTimes();
+        }
+        if (selectingLevel == false) {
 			if (Input.GetKeyDown (KeyCode.Return) || Input.GetKeyDown (KeyCode.KeypadEnter)) {
 				Destroy (GameObject.Find ("Instructions"));
 				selectingLevel = true;
@@ -41,6 +40,19 @@ public class StartGame : MonoBehaviour {
 		}
 	}
 
+    public void showTimes()
+    {
+        bestTimes = levelSelectionMenu.transform.GetChild(3);
+        float totalTime = 0f;
+        for (int i = 1; i <= 5; i++)
+        {
+            float best = PlayerPrefs.GetFloat("Time" + i, 300);
+            totalTime += best;
+            bestTimes.GetChild(i - 1).gameObject.GetComponent<Text>().text = format(best);
+        }
+
+        levelSelectionMenu.transform.GetChild(4).gameObject.GetComponent<Text>().text = "Total: " + format(totalTime);
+    }
     public string format(float time)
     {
         return string.Format("{0:0}:{1:00}.{2:000}", Mathf.Floor(time / 60), time % 60, time * 1000 % 1000);
