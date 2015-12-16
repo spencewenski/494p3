59fa6d8e03703b4723dc2856a9b909fa7e352a82
foodmonkey <jasonphe@umich.edu>
2015-12-15 19:13:31 -0500

    Added timer and best time to all scenes

--- Assets/Scripts/SwitchLevel.cs
+++ Assets/Scripts/SwitchLevel.cs
@@ -4,13 +4,51 @@ using System.Collections;
 public class SwitchLevel : MonoBehaviour {
 
 	public string nextLevel;
-
+    public float level;
+    public float timeTaken;
+    public Times timeScript;
+    public bool bestTime;
+    public bool pause = false;
+    public GameObject bestText;
 	public bool ___________________;
+    void Start()
+    {
+        GameObject go = GameObject.Find("Timer");
+        timeScript = go.GetComponent<Times>();
+        level = timeScript.level;
+        bestText = go.transform.GetChild(0).gameObject;
+    }
 
 	void OnCollisionEnter(Collision other) {
 		if (!(other.gameObject.tag == "Player" || other.gameObject.tag == "cane")) {
 			return;
 		}
-		Application.LoadLevel(nextLevel);
+        float best = PlayerPrefs.GetFloat("Time" + level, 300);
+        timeTaken = timeScript.time;
+        if (timeTaken < best)
+        {
+            bestText.SetActive(true);
+            pause = true;
+            PlayerPrefs.SetFloat("Time" + level, timeScript.time);
+
+        }
+        else if(!pause)
+        {
+            Application.LoadLevel(nextLevel);
+        }
+		
 	}
+    void Update()
+    {
+        if (pause)
+        {
+            Time.timeScale = 0;
+            if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+            {
+                Time.timeScale = 1;
+                Application.LoadLevel(nextLevel);
+            }
+        }
+    }
+
 }
--- Assets/Scripts/Times.cs
+++ Assets/Scripts/Times.cs
@@ -5,22 +5,22 @@ using System.Collections;
 public class Times : MonoBehaviour {
     public float time = 0f;
     public Text text;
-    private string currTime;
+    public string currTime;
     public int level;
-    private string highscore;
+    public string bestTime;
     // Use this for initialization
     void Start () {
         text = GetComponent<Text>();
-        float high = PlayerPrefs.GetFloat("highscore" + level, 0);
-        highscore = string.Format("{0:0}:{1:00}", Mathf.Floor(high / 60), high % 60); ;
+        float best = PlayerPrefs.GetFloat("Time" + level, 300);
+        bestTime = string.Format("{0:0}:{1:00}:{2:000}", Mathf.Floor(best / 60), best % 60, best*1000%1000); ;
 
     }
 	
 	// Update is called once per frame
 	void Update () {
         time += Time.deltaTime;
-        currTime = string.Format("{0:0}:{1:00}", Mathf.Floor(time / 60), time % 60);
-        text.text = currTime + "\nHigh: " + highscore;
+        currTime = string.Format("{0:0}:{1:00}:{2:000}", Mathf.Floor(time / 60), time % 60, time*1000%1000);
+        text.text = "Best: " + bestTime + "\n\t\t   " + currTime;
 	}
 
    
