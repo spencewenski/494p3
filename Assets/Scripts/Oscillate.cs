40aeb9f390aff4008fd821a0eecd8755632a8032
Spencer Ferris <sferris37@gmail.com>
2015-12-18 02:14:26 -0500

    fix oscillate script to match intuition

--- Assets/Scripts/Oscillate.cs
+++ Assets/Scripts/Oscillate.cs
@@ -6,20 +6,22 @@ public class Oscillate : MonoBehaviour {
     public enum OscilateType_e { SINE, INTERPOLATION }
 
     public float height;
-    public float frequency;
+    public float period;
 
     public bool ______________;
+
+    public float originalHeight;
 
 
 	// Use this for initialization
 	void Start () {
-
+        originalHeight = transform.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         Vector3 position = transform.position;
-        position.y += Mathf.Sin(2 * Mathf.PI * frequency * Time.time) * (height * frequency * Time.deltaTime);
+        position.y = originalHeight + (height * Mathf.Sin((2 * Mathf.PI * Time.time) / period));
         transform.position = position;
 	}
 }
